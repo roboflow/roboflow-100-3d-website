4d16df2fa60a05ae9d060c970bc4859ecb961c0c
FrancescoSaverioZuppichini <francesco.zuppichini@gmail.com>
2022-11-04 15:51:07 +0100

    debounce for 5ms

--- static/js/plot.js
+++ static/js/plot.js
@@ -358,7 +358,7 @@ function setUpUIControllers(datasets) {
     let debounceTimeoutId;
     canvas.addEventListener("mousemove", (e) => {
         clearTimeout(debounceTimeoutId);
-        debounceTimeoutId = setTimeout(() => handleMouseMove(e), 10)
+        debounceTimeoutId = setTimeout(() => handleMouseMove(e), 5)
     })
     // reset camera
     const resetCameraBtn = document.querySelector("#reset-camera")
